c6087f713115c84a3fcddf7e5230b4c61c5c7931
egoro <egorobidin@inbox.ru>
2023-02-02 21:25:41 +0300

    lesson 3

--- apps/cart_shop/models.py
+++ apps/cart_shop/models.py
@@ -6,7 +6,7 @@ from apps.cart.models import Cart
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.IntegerField()
+    discount = models.IntegerField(null=True, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='static/shop/images', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
