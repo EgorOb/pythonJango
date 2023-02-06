b6312212ff6d43202cd1884c81944ec2f4996227
Egor <egorobidin@inbox.ru>
2023-02-07 01:37:30 +0300

    static

--- apps/cart_shop/views.py
+++ apps/cart_shop/views.py
@@ -5,6 +5,8 @@ from .models import CartItemShop, Cart, Product
 
 class ViewCart(View):
     def get(self, request):
+        cart = request.session.get('cart', {})
+        print(cart)
         cart_items = CartItemShop.objects.filter(cart__user=request.user)
         data = list(cart_items)
         total_price_no_discount = sum(item.product.price * item.quantity
@@ -21,8 +23,18 @@ class ViewCart(View):
 
 
 def save_product_in_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    print(cart)
+    if not cart:
+        cart_items = CartItemShop.objects.filter(cart__user=request.user)
+        for item in cart_items:
+            cart[item.product.id] = item.quantity
+
     cart_items = CartItemShop.objects.filter(cart__user=request.user,
-                                             product__id=product_id)
+                                            product__id=product_id)
+    cart[product_id] = cart.get(product_id, 0) + 1
+    request.session['cart'] = cart
+
     if cart_items:
         cart_item = cart_items[0]
         cart_item.quantity += 1
@@ -47,7 +59,9 @@ class ViewCartAdd(View):
 
 class ViewCartDel(View):
     def get(self, request, item_id):
+        cart = request.session.get('cart', {})
         cart_item = get_object_or_404(CartItemShop, id=item_id)
+        cart.pop(str(cart_item.product.id))
         cart_item.delete()
         return redirect('cart_shop:cart')
 
