012027353beaba4445591dbb186c8ff8f805b09e
Egor <egorobidin@inbox.ru>
2023-02-06 17:43:08 +0300

    lesson 4_1

--- apps/auth_shop/views.py
+++ apps/auth_shop/views.py
@@ -1,7 +1,10 @@
 from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from apps.cart_shop.models import Cart
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 class Login(View):
@@ -16,6 +19,24 @@ class Login(View):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                print(user)
                 return redirect('home:index')
         return redirect('auth_shop:login')
+
+
+class CreateUserView(View):
+    def get(self, request):
+        return render(request, "auth_shop/create_account.html", )
+
+    def post(self, request):
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            cart = Cart(user=user)
+            user.save()
+            cart.save()
+            login(request, user)
+            return redirect('home:index')
+        return render(request, "auth_shop/create_account.html", context={'errors': form.errors})
