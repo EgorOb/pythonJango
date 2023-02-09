546c42cd263f1f93dd5990954004bfb269110e63
Egor <egorobidin@inbox.ru>
2023-02-09 16:56:58 +0300

    lesson 4_2

--- apps/cart_shop/views.py
+++ apps/cart_shop/views.py
@@ -3,35 +3,28 @@ from django.views import View
 from .models import CartItemShop, Cart, Product
 
 
-class ViewCart(View):
-    def get(self, request):
-        cart = request.session.get('cart', {})
-        print(cart)
-        cart_items = CartItemShop.objects.filter(cart__user=request.user)
-        data = list(cart_items)
-        total_price_no_discount = sum(item.product.price * item.quantity
-                                      for item in data)
-        total_discount = sum(item.product.price * item.product.discount * item.quantity
-                             for item in data if item.product.discount is not None)/100
-        total_sum = total_price_no_discount - total_discount
-        context = {'cart_items': data,
-                   'total_price_no_discount': total_price_no_discount,
-                   'total_discount': total_discount,
-                   'total_sum': total_sum,
-                   }
-        return render(request, 'cart_shop/cart.html', context)
-
-
-def save_product_in_cart(request, product_id):
+def fill_card_in_session(request):
     cart = request.session.get('cart', {})
-    print(cart)
     if not cart:
         cart_items = CartItemShop.objects.filter(cart__user=request.user)
         for item in cart_items:
             cart[item.product.id] = item.quantity
+        request.session['cart'] = cart
+    return cart
 
+
+def fill_id_card_in_session(request):
+    id_cart = request.session.get('id_cart', None)
+    if not id_cart:
+        id_cart = Cart.objects.get(user=request.user).id
+        request.session['id_cart'] = id_cart
+    return id_cart
+
+
+def save_product_in_cart(request, product_id):
+    cart = fill_card_in_session(request)
     cart_items = CartItemShop.objects.filter(cart__user=request.user,
-                                            product__id=product_id)
+                                             product__id=product_id)
     cart[product_id] = cart.get(product_id, 0) + 1
     request.session['cart'] = cart
 
@@ -43,6 +36,27 @@ def save_product_in_cart(request, product_id):
         cart_user = get_object_or_404(Cart, user=request.user)
         cart_item = CartItemShop(cart=cart_user, product=product)
     cart_item.save()
+
+
+class ViewCart(View):
+    def get(self, request):
+        cart = fill_card_in_session(request)
+        if cart:
+            products = Product.objects.filter(id__in=cart.keys())
+            data = [{"product": product, "quantity": cart[str(product.id)], 'id': product.id} for product in products]
+        else:
+            data = []
+        total_price_no_discount = sum(item['product'].price * item['quantity']
+                                      for item in data)
+        total_discount = sum(item['product'].price * item['product'].discount * item['quantity']
+                             for item in data if item['product'].discount is not None) / 100
+        total_sum = total_price_no_discount - total_discount
+        context = {'cart_items': data,
+                   'total_price_no_discount': total_price_no_discount,
+                   'total_discount': total_discount,
+                   'total_sum': total_sum,
+                   }
+        return render(request, 'cart_shop/cart.html', context)
 
 
 class ViewCartBuy(View):
@@ -59,9 +73,11 @@ class ViewCartAdd(View):
 
 class ViewCartDel(View):
     def get(self, request, item_id):
-        cart = request.session.get('cart', {})
-        cart_item = get_object_or_404(CartItemShop, id=item_id)
+        cart = fill_card_in_session(request)
+        cart_id = fill_id_card_in_session(request)
+        cart_item = get_object_or_404(CartItemShop, cart__id=cart_id, product__id=item_id)
         cart.pop(str(cart_item.product.id))
+        request.session["cart"] = cart
         cart_item.delete()
         return redirect('cart_shop:cart')
 
